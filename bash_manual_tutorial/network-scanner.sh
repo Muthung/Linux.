a86d44ffdbb8bf234ad6a450a9e3b772466fb27d
Muthung <muthunguclintn@gmail.com>
2024-04-02 19:40:36 +0300

    created: simple system scanner(linux)

--- bash_manual_tutorial/network-scanner.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "Simple Network-Scanner Web App"
---
+++ bash_manual_tutorial/sisys.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Simple System Scanner"
+
+service_check() {
+    name_service=$1
+    if systemctl is-active --quiet "$name_service"; then
+        echo "$name_service is running."
+    else
+        echo "$name_service is not running. DEFENSE."
+        read -p "Next action or alert for $name_service: " action
+        echo "Taking action: $action"
+    fi
+}
+
+service_check "ssh"
+service_check "apache2"
+service_check "apache2"
+service_check "vsftpd"
+service_check "postfix"
+service_check "dovecot"
+service_check "dovecot"
+service_check "bing9"
+service_check "mysql"
+service_check "xrdp"
+service_check "smbd"
+service_check "nmbd"
