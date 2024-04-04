1aa15b7de007324f09a72229e4bd49ffb250d786
Muthung <muthunguclintn@gmail.com>
2024-04-04 13:41:29 +0300

    fix: removed version check

--- bash_manual_tutorial/sisys.sh
+++ bash_manual_tutorial/sisys.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-figlet "Simple  System Scanner"
+figlet "Simple System Scanner "
+
+# This function (service_check) it analyzes common 
+# services used for remote access whether the service
+# is Running or Down.
 
 service_check() {
     name_service=$1
-    service_version=$(dpkg-query -W -f='${Version}' "$name_service" 2>/dev/null)
     if systemctl is-active --quiet "$name_service"; then
         echo " $name_service is running. "
     else
         echo ""
-        echo -e ">>> $name_service is down. DEFENSE. Version: $service_version"
+        echo -e ">>> $name_service is down. DEFENSE!!!."
         echo ""
-        read -p ">>> Next action or alert for $name_service: " action
+        read -p ">>> Press Enter for the next service: " action
         echo ""
-        echo "Taking action: $action"
+        echo "Checking... Done. $action"
     fi
 }
 
 service_check "SSH"
-service_check "APACHE2"
 service_check "APACHE2"
 service_check "VSFTPD"
 service_check "POSTFIX"
@@ -26,5 +28,4 @@ service_check "DOVECOT"
 service_check "BING9"
 service_check "MySQL"
 service_check "XRDP"
-service_check "SMBD"
 service_check "SMBD"
