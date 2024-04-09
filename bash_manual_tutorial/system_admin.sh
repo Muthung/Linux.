3bd3b717f35da64331361a4c7db64c2c5300edef
Muthung <muthunguclintn@gmail.com>
2024-04-09 06:36:07 +0300

    update: logger.sh

--- bash_manual_tutorial/system_admin.sh
+++ bash_manual_tutorial/system_admin.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
-# Simple SYtem Scanner function.
+# Simple System Scanner function.
+
+# The function (service_check) it analyzes common 
+# services used for remote access whether the service
+# is Running or Down.
 
 function sisys() {
     figlet "Simple System Scanner "
 
-    # This function (service_check) it analyzes common 
-    # services used for remote access whether the service
-    # is Running or Down.
+
 
     service_check() {
         name_service=$1
@@ -32,11 +34,51 @@ function sisys() {
     service_check "MySQL"
     service_check "XRDP"
     service_check "SMBD"
+
+    exit 0
 }
 
 function logger() {
-    echo "Logger..."
+    # Note that there are ather log files in /var/log not affected
+    # by this script.
+
+    DIR_LOG=/var/log
+    ROOT_UID=0      # Only users with $UID 0 have root privileges.
+    LINES=50        # Default number of lines saved.
+    E_XCD=86        # Can't change direcory?.
+    E_NOTROOT=87    # Non-root exit error.
+
+    # Run as root.
+    if [ "$UID" -ne "$ROOT_UID" ]
+    then
+        echo "Must be root to run this script."
+        exit $E_NOTROOT
+    fi
+
+    if [ -n "$1" ]
+    # Test whether command line argument is prresent (non-empty).
+    then
+        lines=$1
+    else
+        lines=$LINES        # Default, if not specified on command-line.
+    fi
+
+    cd $DIR_LOG || {
+        echo "Cannot change to necessary directory. " >&2
+        exit $E_XCD;
+    }
+
+    tail -n $lines messages > mesg.temp         # Saves last section of message log file.
+    mv mesg.temp system_log_file
+
+    cat /dev/null > wtmp        # ': > wtmp' and '> wtmp' have the same effect.
+    echo "Log files successfully cleaned."
+
+    exit 0
+    # A zero return value from the script upon exit indicate success to the shell.
 }
+
+# Quit the program.
 
 function Quit() {
     echo "Done."
